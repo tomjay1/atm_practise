b044cd3632e403274eddddc06e0152f353443f71
OKPETE TOMJAY <81090931+tomjay1@users.noreply.github.com>
2021-05-05 00:54:03 +0100

    the main atm practise and body of work
    works well with new features

--- atm_practise.py
+++ atm_practise.py
@@ -1,27 +1,27 @@
 #ATM PRACTISE
 
+#print ('input login details')
+
+#name = input ('what is your name? \n')
+#allowedusers = ['tomjay','seyi','mike','efe']
+#allowedpassword ='tominio'
+#if(name in allowedusers):
+    #password = input ('your password \n')
+
+    #if(password in allowedpassword):
+        #print ('welcome %s' 'name')
+    #else:
+        #print('password incorrect, please try again')
+    
+
+#else:
+    #print ('name not found, please try again')
+    
 print ('input login details')
 
 name = input ('what is your name? \n')
-allowedusers = ['tomjay','seyi','mike','efe']
-allowedpassword ='tominio'
-if(name in allowedusers):
-    password = input ('your password \n')
-
-    if(password in allowedpassword):
-        print ('welcome %s' 'name')
-    else:
-        print('password incorrect, please try again')
-    
-
-else:
-    print ('name not found, please try again')
-    
-print ('input login details')
-
-name = input ('what is your name? \n')
-allowedusers = ['tomjay','seyi','mike','efe']
-allowedpassword =['tominio','calida','wifi', 'comein']
+allowedusers = ['tomjay','bimpe','mike','efe']
+allowedpassword =['tmoney','bmoney','wifi', 'comein']
 
 if(name in allowedusers):
     password = input ('your password \n')
@@ -29,33 +29,74 @@ if(name in allowedusers):
 
     if(password in allowedpassword[userID]):
         print ('welcome %s' % name)
-       
-        print ('1.withdrawal')
-        print ('2. deposite')
-        print ('3.any complain')
-        selectedoption = int(input ('please select an option \n'))
-
-        if(selectedoption == 1):
-            print('input withdral amount \n')            
-        elif(selectedoption == 2):
-            print('input deposite amount \n ')
-        elif(selectedoption == 3):
-            print('input complain? \n ')
+        import datetime
+        now = datetime.datetime.now()   
+        #print ('current date and time :')
+        print (now.strftime('%Y/%m/%d %H:%M:%S'))
+        print ('current balance #50000 ')
+        print ('proceed and select an option')
+        print ('1. withdrawal')
+        print ('2. deposit')
+        print ('3. any complain')
+        selectedoption = int(input ('input selected an option \n'))
+                #withdrawal
+        if(selectedoption ==1):
+            print ('select amount to withdraw')
+            print  ('1. #10000')
+            print  ('2. #20000')
+            print  ('3. #30000')
+            selectedOption = int(input ('input selected option \n'))
+                
+            if(selectedOption ==1):
+             print ('dispensing... take your cash,\ current balance #40000, \ thank you')
+            elif(selectedOption ==2):
+             print ('dispensing... take your cash,\ current balance #30000, \ thank you')
+            elif (selectedOption ==3):
+             print ('dispensing... take your cash,\ current balance #20000, \ thank you')
+                #deposit
+        elif(selectedoption ==2):
+            print ('select amount to deposit')
+            print  ('1. #10000')
+            print  ('2. #15000')
+            print  ('3. #25000')
+            selectedOption = int(input ('input selected option \n'))
+                
+            if(selectedOption ==1):
+             print ('account credited \ current balance #60000,\ thank you')
+            elif(selectedOption ==2):
+             print ('account credited \ current balance #65000,\ thank you ')
+            elif (selectedOption ==3):
+             print ('account credited \ current balance #75000,\ thank you')
+                #complain
+        elif (selectedoption ==3):
+            print ('what issue will you like to report')
+            print  ('1. lost atm card/lost token')
+            print  ('2. pos purchase error ')
+            print  ('3. unathorized transaction')
+            selectedOption = int(input ('input selected option \n'))
+                
+            if(selectedOption ==1):
+             print ('kindly visit the nearest branch to request for a new device, thanks')
+            elif(selectedOption ==2):
+             print ('kindly visit our website, thank you')
+            elif (selectedOption ==3):
+             print ('kindly get a police report and visit the nearest branch, thank you')
+            
         else:
             print ('invalid option selected, please try again')
     else:
-        print('password incorrect, please try again')
+            print('password incorrect, please try again')
 else:
     print ('name not found, please try again')
 
-print  ('1. #5000')
-print  ('2. #10000')
-print  ('3. #15000')
-selectedOption = int(input ('plases select an option \n'))
+#print  ('1. #5000')
+#print  ('2. #10000')
+#print  ('3. #15000')
+#selectedOption = int(input ('input selected option \n'))
 
-if(selectedOption ==1):
-    print ('loading... take your cash, thank you')
-elif(selectedOption ==2):
-        print ('loading... take cash, thanks')
-elif (selectedOption ==3):
-        print ('accepted payment')
+#if(selectedOption ==1):
+  #  print ('loading... take your cash,\ thank you')
+#elif(selectedOption ==2):
+   #     print ('loading...\ current balance #20000,\ thanks')
+#elif (selectedOption ==3):
+ #       print ('thank you for contacting us')
